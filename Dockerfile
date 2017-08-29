6fc2dfec57c4e58b9b5eab6d1825d24c2478ff35
EgoFelix <git@egofelix.de>
2017-08-29 21:47:11 +0200

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+#FROM alpine:latest
+FROM alpine:edge
+
+MAINTAINER EgoFelix <docker@egofelix.de>
+
+# Install Certbot
+RUN apk add --no-cache \
+    bash \
+	certbot
+
+# Install Entrypoint
+COPY ./run.sh /run.sh
+RUN chmod +x /run.sh
+
+CMD ["/run.sh"]
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -z ${renew+x} ]; then
+
+  if [ -z ${email+x} ]; then echo "Fatal: administrator email address must be specified with the environment variable named 'email'"; exit 1; fi
+  if [ -z ${domains+x} ]; then echo "Fatal: domains must be specified with the environment variable named 'domains'"; exit 1; fi
+
+  IFS=',' read -ra ADDR <<< "$domains"
+  for domain in "${ADDR[@]}"; do
+    certbot certonly --verbose --noninteractive --quiet --standalone --preferred-challenges http --agree-tos --email="${email}" -d "${domain}"
+  done
+  
+else
+
+  certbot renew
+  
+fi
