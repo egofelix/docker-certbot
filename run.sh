22fd5a5e518da6cd7cb749403591473c9ff75a3a
Felix Wederz <felix@egofelix.de>
2018-03-02 11:13:08 +0100

    Changed renew procedure

--- run.sh
+++ run.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -z ${renew+x} ]; then
+if [ -z ${email+x} ]; then echo "Fatal: administrator email address must be specified with the environment variable named 'email'"; exit 1; fi
+if [ -z ${domains+x} ]; then echo "Fatal: domains must be specified with the environment variable named 'domains'"; exit 1; fi
 
-  if [ -z ${email+x} ]; then echo "Fatal: administrator email address must be specified with the environment variable named 'email'"; exit 1; fi
-  if [ -z ${domains+x} ]; then echo "Fatal: domains must be specified with the environment variable named 'domains'"; exit 1; fi
-
-  IFS=',' read -ra ADDR <<< "$domains"
-  for domain in "${ADDR[@]}"; do
-    certbot certonly --verbose --noninteractive --quiet --standalone --preferred-challenges http-01 --agree-tos --email="${email}" -d "${domain}"
-  done
-
-else
-
-  certbot renew --verbose --noninteractive --quiet --standalone --preferred-challenges http-01 --agree-tos
-
-fi
+IFS=',' read -ra ADDR <<< "$domains"
+for domain in "${ADDR[@]}"; do
+  certbot certonly --verbose --noninteractive --standalone --preferred-challenges http-01 --agree-tos --email="${email}" -d "${domain}"
+done
