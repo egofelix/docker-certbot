d06478e0da64afd1fb92e0328ede47c8425e2b52
egofelix <github@egofelix.de>
2018-11-17 16:57:54 +0100

    Removed Obsolete Files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z ${email+x} ]; then echo "Fatal: administrator email address must be specified with the environment variable named 'email'"; exit 1; fi
-if [ -z ${domains+x} ]; then echo "Fatal: domains must be specified with the environment variable named 'domains'"; exit 1; fi
-
-IFS=',' read -ra ADDR <<< "$domains"
-for domain in "${ADDR[@]}"; do
-  certbot certonly --verbose --noninteractive --standalone --preferred-challenges http-01 --agree-tos --email="${email}" -d "${domain}"
-done
